f5c7b892f65446106d100d12372f3b524f6f49ef
grafica <paolo@monema.it>
2025-01-23 16:57:04 +0100

    blur animazione in home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className="blur">
         <div className="scanlines"></div>
         {children}
       </body>
